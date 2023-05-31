c0632c47caa7f297aeb1ad2a3f2e95071930a97b
Tayyab Tariq <tayyabashraf22@gmail.com>
2023-05-31 22:05:28 +0500

    First Commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api/users', userRoutes);
+app.use(notFound);
+app.use(errorHandler);
+
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
